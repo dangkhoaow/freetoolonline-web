3db8d5dc78b80b35ac632c4e19117a8c0b680325
K Tran <dangkhoaow@gmail.com>
2026-07-27 04:48:58 +0700

    new-tool-discovery fire538: URL Slug Generator + EN/locale companion guides
    Client-side slugify (NFD + Vietnamese d-stroke) at /developer-tools/slug-generator.html
    with when / step-by-step / vs-alternatives guides in EN+pt/es/vi/id/de.

--- source/web/src/main/webapp/static/script/related-tools.js
+++ source/web/src/main/webapp/static/script/related-tools.js
@@ -822,6 +822,7 @@ try {
     { title: "User Agent Parser", url: "https://freetoolonline.com/developer-tools/user-agent-parser.html", include: !1, tags: "developer" },
     { title: "Random Team Generator", url: "https://freetoolonline.com/utility-tools/random-team-generator.html", include: !1, tags: "utility" },
     { title: "Cron Expression Parser", url: "https://freetoolonline.com/developer-tools/cron-expression-parser.html", include: !1, tags: "developer" },
+    { title: "URL Slug Generator", url: "https://freetoolonline.com/developer-tools/slug-generator.html", include: !1, tags: "developer" },
     { title: "Time Zone Converter", url: "https://freetoolonline.com/utility-tools/time-zone-converter.html", include: !1, tags: "utility" },
     { title: "Signature Maker", url: "https://freetoolonline.com/image-tools/signature-maker.html", include: !1, tags: "image-editing" },
     { title: "Meme Maker", url: "https://freetoolonline.com/image-tools/meme-maker.html", include: !1, tags: "image-editing" },
